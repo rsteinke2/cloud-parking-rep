e24b579282ed75ed3b55bd9dd42de8193c35c7b3
Ricardo <ricardosteinke2@gmail.com>
2022-10-28 15:07:45 -0300

    Estrutura Inicial

---
+++ src/main/java/one/digitalinnovation/parking/CloudParkingRepApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudParkingRepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudParkingRepApplication.class, args);
+	}
+
+}
