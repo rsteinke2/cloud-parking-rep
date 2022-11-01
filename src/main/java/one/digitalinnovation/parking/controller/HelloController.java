27b384154bf89351a9927be3c808a72e1ba69364
Ricardo <ricardosteinke2@gmail.com>
2022-11-01 16:46:15 -0300

    Primeiro endpoint de lista.

--- src/main/java/one/digitalinnovation/parking/controller/HelloController.java
+++ src/main/java/one/digitalinnovation/parking/controller/HelloController.java
@@ -1,4 +1,4 @@
-package one.digitalinnovation.parking.HelloController;
+package one.digitalinnovation.parking.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
