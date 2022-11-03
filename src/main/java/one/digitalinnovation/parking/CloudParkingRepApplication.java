5dcb20c810419b3a8b669e4ce586dd8591db2d16
Ricardo <ricardosteinke2@gmail.com>
2022-11-03 14:05:39 -0300

    Adicionado SWAGGER (dep. Springfox starter.

--- src/main/java/one/digitalinnovation/parking/CloudParkingRepApplication.java
+++ src/main/java/one/digitalinnovation/parking/CloudParkingRepApplication.java
@@ -2,6 +2,7 @@ package one.digitalinnovation.parking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class CloudParkingRepApplication {
--- src/main/java/one/digitalinnovation/parking/service/ParkingService.java
+++ src/main/java/one/digitalinnovation/parking/service/ParkingService.java
@@ -3,6 +3,7 @@ package one.digitalinnovation.parking.service;
 import one.digitalinnovation.parking.model.Parking;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,8 +16,11 @@ public class ParkingService {
     private static Map<String, Parking> parkingMap = new HashMap();
     static  {
         var id = getUUID();
+        var id1 = getUUID();
         Parking parking = new Parking(id, "DMS-1111", "SC", "CELTA", "PRETO");
+        Parking parking1 = new Parking(id1, "WAS-1111", "SP", "VW GOL", "VERMELHO");
         parkingMap.put(id, parking);
+        parkingMap.put(id1, parking1);
     }
     public List<Parking> findAll(){
         return parkingMap.values().stream().collect(Collectors.toList());
@@ -24,7 +28,17 @@ public class ParkingService {
     private static String getUUID(){
         return UUID.randomUUID().toString().replace("-", "");
     }
+    public Parking findById(String id){
+        return parkingMap.get(id);
+
+    }
 
 
-
+    public Parking create(Parking parkingCreate) {
+        String uuid = getUUID();
+        parkingCreate.setId(uuid);
+        parkingCreate.setEntryDate(LocalDateTime.now());
+        parkingMap.put(uuid, parkingCreate);
+        return parkingCreate;
+    }
 }
